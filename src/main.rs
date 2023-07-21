38886058ff192af7fd3b53d744bc78d9bd8f9e8b
Dan (dx2718) <dx2718@gmail.com>
2023-07-20 21:57:33 -0400

    waiting on regex-chunker to improve

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use std::{
-    io::{stdin, stdout, BufRead, Write},
-    process::{Command, Stdio},
-    sync::mpsc::{sync_channel, Receiver, SyncSender},
-    thread::spawn,
+    error::Error,
+    io::{BufRead, stdin, stdout, Write},
+    process::Stdio,
 };
 
-use regex_chunker::ByteChunker;
+use tokio::{
+    process::Command,
+    runtime,
+    sync::mpsc::{channel, Sender, Receiver},
+};
+use tokio_stream::StreamExt;
+use regex_chunker::stream::ByteChunker;
+
+static SHELL: &str = "sh";
+#[cfg(unix)]
+static NEWLINE: &[u8] = &[10];
+#[cfg(windows)]
+static NEWLINE: &[u8] = &[13, 10];
 
 fn get_commands() -> Result<Vec<String>, String> {
     let mut v: Vec<String> = Vec::new();
     for line in stdin().lock().lines().map(|r| r.unwrap()) {
-        println!("{:?}", line.trim().as_bytes());
         if line.trim() == "" {
             break;
         } else {
@@ -20,42 +30,75 @@ fn get_commands() -> Result<Vec<String>, String> {
     Ok(v)
 }
 
-fn main() {
-    let cmds = get_commands().unwrap();
+async fn wrapped_spawner(cmd: &str, tx: Sender<Vec<u8>>) -> Result<(), Box<dyn Error>> {
+    let mut child = Command::new(SHELL)
+        .arg("-c")
+        .arg(cmd)
+        .stdout(Stdio::piped())
+        .spawn()?;
 
-    let mut outputs: Vec<_> = cmds
-        .into_iter()
-        .map(|cmd| {
-            let (tx, rx) = sync_channel::<Vec<u8>>(0);
-            spawn(move || {
-                let mut child = Command::new("sh")
-                    .arg("-c")
-                    .arg(&cmd)
-                    .stdout(Stdio::piped())
-                    .spawn()
-                    .unwrap();
-                let stdout = child.stdout.take().unwrap();
-                let chunker = ByteChunker::new(stdout, r#"\r?\n"#).unwrap();
-                for chunk in chunker {
-                    let v = chunk.unwrap();
-                    eprintln!("read line: {:?}", &String::from_utf8_lossy(&v));
-                    tx.send(v).unwrap();
-                }
-                let _ = child.wait().unwrap();
-            });
-            rx
-        })
-        .collect();
+    let stdout = child.stdout.take().ok_or("no stdout handle!")?;
 
-    loop {
+    let mut chunker = ByteChunker::new(stdout, r#"\r?\n"#)?;
+    while let Some(chunk) = chunker.next().await {
+        tx.send(chunk?).await?;
+    }
+    child.wait().await?;
+
+    Ok(())
+
+}
+
+async fn spawn_process(cmd: String, tx: Sender<Vec<u8>>) {
+    if let Err(e) = wrapped_spawner(&cmd, tx).await {
+        eprintln!("error in child process {:?}: {}", &cmd, &e);
+    }
+}
+
+async fn read_loop(mut outputs: Vec<Receiver<Vec<u8>>>) -> std::io::Result<()> {
+    let mut stdout = stdout().lock();
+    let mut done: bool = false;
+
+    while !done {
         for rx in outputs.iter_mut() {
-            match rx.recv() {
-                Ok(v) => {
-                    eprintln!("main thread rec'd: {:?}", )
-                    stdout().write_all(&v).unwrap(),
+            match rx.recv().await {
+                Some(v) => {
+                    stdout.write_all(&v)?;
+                    stdout.write_all(NEWLINE)?;
                 },
-                Err(_) => break,
+                None => { done = true; },
             }
         }
+        stdout.flush()?;
     }
+
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    let cmds = get_commands().unwrap();
+
+    let rt = runtime::Builder::new_current_thread()
+        .enable_io()
+        .build().map_err(|e| format!("unable to build runtime: {}", &e))?;
+
+    rt.block_on(async {
+
+        let mut outputs: Vec<_> = cmds
+            .into_iter()
+            .map(|cmd| {
+                let (tx, rx) = channel::<Vec<u8>>(1);
+                tokio::spawn(async move {
+                    spawn_process(cmd, tx).await;
+                });
+                rx
+            })
+            .collect();
+
+        if let Err(e) = read_loop(outputs).await {
+            eprintln!("{}", &e);
+        }
+    });
+
+    Ok(())
 }
