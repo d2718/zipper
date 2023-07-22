76b4e0554d499a6d0b6e11346137647191f32f26
Dan <dx2718@gmail.com>
2023-07-22 09:38:43 -0400

    Added --exit and --thread main options, as well as --skip and --take command options; added some documentation

--- src/cmds.rs
+++ src/cmds.rs
@@ -16,36 +16,31 @@ static SHELL: &str = "sh";
 static SEPARATOR: &str = " || ";
 static DELIMITER: &str = r#"\r?\n"#;
 
-// Find the position (of the start of) the sublsice `needle` in the
-// slice `haystack`. Returns `None` if not present.
-/* fn find_subslice<T: PartialEq>(haystack: &[T], needle: &[T]) -> Option<usize> {
-    if needle.len() > haystack.len() {
-        return None;
-    }
+/**
+Optons to specify how the output of a specific subcommand is treated.
 
-    for (n, w) in haystack.windows(needle.len()).enumerate() {
-        if w == needle {
-            return Some(n);
-        }
-    }
-
-    None
-} */
-
-/// Optons to specify how the output of a specific subcommand is treated.
-#[derive(Debug, Parser)]
+These values are derived from the pre-command portion of a command
+line, if present.
+*/
+#[derive(Clone, Debug, Parser)]
 pub struct CmdOpts {
+    /// regex to use as a delimiter instead of a newline
     #[arg(short, long)]
     delimiter: Option<String>,
-    #[arg(long)]
-    max: Option<usize>,
+    /// skip this many lines before echoing output
+    #[arg(short, long)]
+    skip: Option<usize>,
+    /// only output this many lines, then stop
+    #[arg(short, long)]
+    take: Option<usize>,
 }
 
 impl Default for CmdOpts {
     fn default() -> Self {
         CmdOpts {
             delimiter: None,
-            max: None,
+            skip: None,
+            take: None,
         }
     }
 }
@@ -54,32 +49,72 @@ impl CmdOpts {
     fn parse(snip: &str) -> Result<CmdOpts, String> {
         let chunks =
             shlex::split(snip).ok_or_else(|| "unable to parse option snippet".to_string())?;
-        let d = String::new();
-        CmdOpts::try_parse_from([d].into_iter().chain(chunks.into_iter()))
+        // shlex is for parsing command lines, so it assumes that the first
+        // chunk is the name of the executable. In this context, we don't
+        // have an executable, so we have to start with a dummy chunk.
+        let dummy = String::new();
+        CmdOpts::try_parse_from([dummy].into_iter().chain(chunks.into_iter()))
             .map_err(|e| format!("{}", &e))
     }
 }
 
+/**
+Specify to the task spawner the tokio threading model to use.
+
+This is used to control the behavior of [`Cmd::spawn`].
+*/
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Threading {
+    /// Spawn tasks using `tokio::task::spawn_local`
+    Local,
+    /// Spawn tasks using `tokio::spawn`
+    Multi,
+}
+
+/**
+All the information required to run one of the commands whose output
+will be interleaved.
+*/
 #[derive(Debug)]
 pub struct Cmd {
+    /// Information from the line prefix options.
     opts: CmdOpts,
+    /// Command string to pass to the shell.
     exec: String,
 }
 
+/// Read the output of a command and pass it chunk by chunk down
+/// the provided channel.
 async fn read_loop<A>(
     mut chunker: ByteChunker<A>,
+    opts: CmdOpts,
     tx: Sender<Vec<u8>>,
 ) -> Result<(), Box<dyn Error>>
 where
     A: AsyncRead + Unpin,
 {
-    while let Some(chunk) = chunker.next().await {
-        tx.send(chunk?).await?;
+    if let Some(n) = opts.skip {
+        for _ in 0..n {
+            if let None = chunker.next().await {
+                return Ok(());
+            }
+        }
+    }
+    if let Some(n) = opts.take {
+        let mut chunker = chunker.take(n);
+        while let Some(chunk) = chunker.next().await {
+            tx.send(chunk?).await?;
+        }
+    } else {
+        while let Some(chunk) = chunker.next().await {
+            tx.send(chunk?).await?;
+        }
     }
     Ok(())
 }
 
 impl Cmd {
+    /// Read a line from stdin and parse it into a command to run.
     pub fn from_line(line: &str) -> Result<Cmd, String> {
         // This line is pretty gross, but it works. We have to supply
         // the turbofish to collect into a Vec, then the slicing index
@@ -97,7 +132,8 @@ impl Cmd {
         Ok(Cmd { opts, exec })
     }
 
-    pub fn spawn(&self) -> Result<Receiver<Vec<u8>>, Box<dyn Error>> {
+    /// Spawn the represented command under the given `Threading` mode.
+    pub fn spawn(&self, t: Threading) -> Result<Receiver<Vec<u8>>, Box<dyn Error>> {
         let mut child = Command::new(SHELL)
             .arg("-c")
             .arg(&self.exec)
@@ -106,22 +142,40 @@ impl Cmd {
 
         let stdout = child.stdout.take().ok_or("no stdout handle!")?;
 
-        let chunker = ByteChunker::new(stdout, self.opts.delimiter.as_deref().unwrap_or(DELIMITER))?;
+        let chunker =
+            ByteChunker::new(stdout, self.opts.delimiter.as_deref().unwrap_or(DELIMITER))?;
         let (tx, rx) = channel::<Vec<u8>>(1);
         let self_debug_string = format!("{:?}", &self);
+        let opts = self.opts.clone();
 
-        tokio::spawn(async move {
-            if let Err(e) = read_loop(chunker, tx).await {
-                eprintln!("error reading output of {:?}: {}", &self_debug_string, &e);
+        match t {
+            Threading::Local => {
+                tokio::task::spawn_local(async move {
+                    if let Err(e) = read_loop(chunker, opts, tx).await {
+                        eprintln!("error reading output of {:?}: {}", &self_debug_string, &e);
+                    }
+                    if let Err(e) = child.wait().await {
+                        eprintln!(
+                            "error waiting for process {:?} to finish: {}",
+                            &self_debug_string, &e
+                        );
+                    }
+                });
             }
-            if let Err(e) = child.wait().await {
-                eprintln!(
-                    "error waiting for process {:?} to finish: {}",
-                    &self_debug_string, &e
-                );
-            }
-        });
-
+            Threading::Multi => {
+                tokio::spawn(async move {
+                    if let Err(e) = read_loop(chunker, opts, tx).await {
+                        eprintln!("error reading output of {:?}: {}", &self_debug_string, &e);
+                    }
+                    if let Err(e) = child.wait().await {
+                        eprintln!(
+                            "error waiting for process {:?} to finish: {}",
+                            &self_debug_string, &e
+                        );
+                    }
+                });
+            },
+        }
         Ok(rx)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,45 @@
 mod cmds;
 
-use std::io::{stdin, stdout, BufRead, Write};
+use std::{error::Error, io::{stdin, stdout, BufRead, Write}};
 
-use tokio::{
-    runtime,
-    sync::mpsc::Receiver,
-};
+use clap::{Parser, ValueEnum};
+use tokio::{runtime, sync::mpsc::Receiver, task::LocalSet};
 
-use cmds::Cmd;
+use cmds::{Cmd, Threading};
 
-static SHELL: &str = "sh";
 #[cfg(unix)]
 static NEWLINE: &[u8] = &[10];
 #[cfg(windows)]
 static NEWLINE: &[u8] = &[13, 10];
 
+/// Controls how zipper behaves when commands terminate after
+/// different amounts of output.
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, ValueEnum)]
+enum Finished {
+    /// Stop when first command terminates.
+    #[default]
+    Terminate,
+    /// Ignore terminated commands.
+    Ignore,
+    /// Insert blank lines for commands that have terminated.
+    Blank,
+}
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Cfg {
+    /// Specify behavior on command termination.
+    #[arg(short, long, value_enum, default_value_t = Finished::Terminate)]
+    exit: Finished,
+    /// Use more threads.
+    #[arg(short, long, default_value_t = false)]
+    threads: bool,
+}
+
+/**
+Read from stdin until EOF or a blank line, and return a Vec of
+`Cmd` structs to run.
+*/
 fn get_commands() -> Result<Vec<Cmd>, String> {
     let mut v: Vec<Cmd> = Vec::new();
     for (n, line_res) in stdin().lock().lines().enumerate() {
@@ -32,53 +57,99 @@ fn get_commands() -> Result<Vec<Cmd>, String> {
     Ok(v)
 }
 
-async fn read_loop(mut outputs: Vec<Receiver<Vec<u8>>>) -> std::io::Result<()> {
-    let mut stdout = stdout().lock();
-    let mut done: bool = false;
+/**
+Repeatedly iterate through the output channels of the commands,
+writing lines from each to stdout.
+*/
+async fn read_loop(mut outputs: Vec<Receiver<Vec<u8>>>, finished: Finished) -> std::io::Result<()> {
+    let mut buff: Vec<u8> = Vec::new();
+    let mut dones: Vec<bool> = vec![false; outputs.len()];
+    let exit_condition: Vec<bool> = vec![true; outputs.len()];
 
-    while !done {
-        for rx in outputs.iter_mut() {
+    'writeloop: while &dones != &exit_condition {
+        for (n, rx) in outputs.iter_mut().enumerate() {
             match rx.recv().await {
                 Some(v) => {
-                    stdout.write_all(&v)?;
-                    stdout.write_all(NEWLINE)?;
+                    buff.write_all(&v)?;
+                    buff.write_all(NEWLINE)?;
                 }
                 None => {
-                    done = true;
+                    dones[n] = true;
+                    match finished {
+                        Finished::Terminate => break 'writeloop,
+                        Finished::Ignore => { /* do nothing */ }
+                        Finished::Blank => {
+                            buff.write_all(NEWLINE)?;
+                        }
+                    }
                 }
             }
         }
+        let mut stdout = stdout().lock();
+        stdout.write_all(&buff)?;
         stdout.flush()?;
+        buff.clear();
     }
 
     Ok(())
 }
 
-fn main() -> Result<(), String> {
-    let cmds = get_commands().unwrap();
-
-    let rt = runtime::Builder::new_current_thread()
+/// Spawn commands and interleave their output using tokio's default
+/// (multh-threaded) task scheduler.
+fn run_threaded(cmds: Vec<Cmd>, cfg: Cfg) -> Result<(), Box<dyn Error>> {
+    let rt = runtime::Builder::new_multi_thread()
         .enable_io()
-        .build()
-        .map_err(|e| format!("unable to build runtime: {}", &e))?;
-
+        .build()?;
+    
     rt.block_on(async {
-        let outputs: Vec<_> = cmds
-            .into_iter()
+        let outputs: Vec<_> = cmds.into_iter()
             .enumerate()
-            .filter_map(|(n, cmd)| match cmd.spawn() {
+            .filter_map(|(n, cmd)| match cmd.spawn(Threading::Multi) {
                 Ok(rx) => Some(rx),
                 Err(e) => {
                     eprintln!("error spawning process {} {:?}: {}", &n, &cmd, &e);
                     None
-                }
-            })
-            .collect();
+                },
+            }).collect();
 
-        if let Err(e) = read_loop(outputs).await {
-            eprintln!("{}", &e);
-        }
-    });
+        read_loop(outputs, cfg.exit).await  
+    }).map_err(|e| e.into())
+}
 
-    Ok(())
+/// Spawn commands and interleave their output using tokio's single-threaded
+/// task scheduler. This does not guarantee strict single-threaded behavior.
+/// Even if it did, each command still needs to run in its own process.
+fn run_local(cmds: Vec<Cmd>, cfg: Cfg) -> Result<(), Box<dyn Error>> {
+    let rt = runtime::Builder::new_current_thread()
+        .enable_io()
+        .build()?;
+    
+    rt.block_on(async {
+        let local = LocalSet::new();
+
+        local.run_until(async move {
+            let outputs: Vec<_> = cmds.into_iter()
+                .enumerate()
+                .filter_map(|(n, cmd)| match cmd.spawn(Threading::Local) {
+                    Ok(rx) => Some(rx),
+                    Err(e) => {
+                        eprintln!("error spawning process {} {:?}: {}", &n, &cmd, &e);
+                        None
+                    },
+                }).collect();
+
+            read_loop(outputs, cfg.exit).await
+        }).await
+    }).map_err(|e| e.into())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cfg = Cfg::parse();
+    let cmds = get_commands().unwrap();
+
+    if cfg.threads {
+        run_threaded(cmds, cfg)
+    } else {
+        run_local(cmds, cfg)
+    }
 }
